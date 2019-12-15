d2d73f0d288e66ea45701bd299d323010ff2bd56
miyabi625 <miyabi625@gmail.com>
2019-12-16 01:18:18 +0900

    アップ漏れ

--- 191212/data_load.py
+++ 191212/data_load.py
@@ -6,30 +6,71 @@ from sklearn.preprocessing import LabelEncoder
 
 class DataLoad:
     ####################################################
+    # 定数宣言
+    ####################################################
+    #FILE_TRAIN_CSV = './input/sales_train.csv'
+    FILE_TRAIN_CSV = './input/sales_train_v2.csv'
+    #FILE_TEST_CSV = './input/test.csv'
+    FILE_TEST_CSV = './input/test2.csv'
+
+    ####################################################
     # ログ宣言
     ####################################################
     log = logging.getLogger(__name__)
     logger.setLogger(log)
     
     # constructor
-    def __init__(self, file_path):
-        self.log.info('init start')
+    def __init__(self):
+        #constructorでは特に処理を行わない
+        pass
+    
+    #トレーニングデータの読み込み
+    def read_train_csv(self):
+        self.log.info('read_train_csv start')
+        
+#date	date_block_num	shop_id	item_id	item_price	item_cnt_day
+#14.01.2013	0	2	11330	149	1
 
-        # Load training data
-        tmp_df = pd.read_csv(file_path, header=0)
-        tmp_df["date_year"] = tmp_df['date'].str[6:10]
-        tmp_df["date_month"] = tmp_df['date'].str[3:5]
-        tmp_df = tmp_df.drop("date", axis=1)
-        tmp_df = tmp_df.drop("item_id", axis=1)
-
-        #お試しで年月で集計してみる
-        tmp_df = tmp_df.groupby(["date_year","date_month"]).agg({"item_price": "mean",
-                    "item_cnt_day":"sum"})
+        ## Load training data
+        tmp_df = pd.read_csv(self.FILE_TRAIN_CSV, header=0,
+            dtype = {
+                'date':'str',
+                'date_block_num':'int',
+                'shop_id':'int',
+                'item_id':'int',
+                'item_price':'float',
+                'item_cnt_day':'float'})
+        
+        tmp_df = tmp_df.groupby(['date_block_num','shop_id','item_id'], as_index=False).agg({
+            #'item_price':np.mean,
+            'item_cnt_day':np.sum})
 
         self.df = tmp_df
 
-        self.log.info('init end')
+        self.log.info('read_train_csv end')
 
-    # 該当項目の取得
-    def getValues(self):
-        return self.df.values
+    #トレーニングデータの読み込み
+    def read_test_csv(self):
+        self.log.info('read_test_csv start')
+        
+#ID	shop_id	item_id
+#20400	2	5037
+
+        ## Load test data
+        tmp_df = pd.read_csv(self.FILE_TEST_CSV, header=0,
+            dtype = {
+                'ID':'str',
+                'shop_id':'int',
+                'item_id':'int'})
+
+        self.df_test = tmp_df
+
+        self.log.info('read_test_csv end')
+
+    # トレーニングデータの取得
+    def getTrainValues(self):
+        return self.df
+        
+    # テストデータの取得
+    def getTestValues(self):
+        return self.df_test
